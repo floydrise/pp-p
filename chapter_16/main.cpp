ba69090c5ada63f21f91535aafd205f14b4412d9
Stefan <floydrise@gmail.com>
2025-09-04 18:44:03 +0300

    Add drill 2

--- chapter_16/main.cpp
+++ chapter_16/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <print>
-int ga[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+#include <vector>
 
-void f(const int *arr, const int n) {
-    int la[10];
-    std::cout << "la values: ";
-    for (int i = 0; i < n; ++i) {
-        la[i] = arr[i];
-        std::print("{} ", la[i]);
+std::vector<int> gv{1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+void f(const std::vector<int> &v) {
+    std::vector<int> lv(10);
+    for (int i = 0; i < lv.size(); ++i) {
+        lv[i] = v[i];
+        std::print("{} ", lv[i]);
         if (i == 9) std::cout << '\n';
     }
-    int *p = new int[10];
-    std::cout << "*p values: ";
-    for (int i = 0; i < n; ++i) {
-        p[i] = arr[i];
-        std::print("{} ", p[i]);
+    std::vector<int> lv2 = v;
+    for (int i = 0; i < lv2.size(); ++i) {
+        std::print("{} ", lv2[i]);
         if (i == 9) std::cout << '\n';
     }
-    delete[] p;
-};
+}
 
 int factorial(int n) {
     if (n == 0) return 1;
@@ -31,10 +29,10 @@ int factorial(int n) {
 }
 
 int main() {
-    f(ga, 10);
-    int aa[10];
+    f(gv);
+    std::vector<int> vv(10);
     for (int i = 0; i < 10; ++i) {
-        aa[i] = factorial(i + 1);
+        vv[i] = factorial(i + 1);
     }
-    f(aa, 10);
+    f(vv);
 }
